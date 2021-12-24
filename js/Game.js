9e98b1ed8e2ccaeb13f1bef4a8f43bdd63822b38
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-12-24 12:50:44 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -59,11 +59,11 @@ class Game {
 
   showLife() {
     push();
-    image(lifeImage, width / 2 - 130, height - player.positionY - 400, 20, 20);
+    image(lifeImage, width / 2 - 130, 40, 20, 20);
     fill("white");
-    rect(width / 2 - 100, height - player.positionY - 400, 185, 20);
+    rect(width / 2 - 100,40, 185, 20);
     fill("#f50057");
-    rect(width / 2 - 100, height - player.positionY - 400, player.life, 20);
+    rect(width / 2 - 100, 40, player.life, 20);
     noStroke();
     pop();
   }
